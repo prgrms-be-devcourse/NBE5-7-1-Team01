07015dc699b8fd36e61239f593bad3251d800d22
dbogym <ko07073@gmail.com>
2025-04-25 15:34:35 +0900

    fix(item): 임의로 사용한 item 관련 파일 삭제

--- src/main/java/io/binary/coffeenotfound_404/dao/ItemsRepository.java
+++
@@ -1,7 +0,0 @@
-package io.binary.coffeenotfound_404.dao;
-
-import io.binary.coffeenotfound_404.domain.Items;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemsRepository extends JpaRepository<Items, Long> {
-}
